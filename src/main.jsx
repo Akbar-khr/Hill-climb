c0ab61a3c74541beaa413c10aae095ede8681af0
Akbar-khr <khrisnaakbar01@gmail.com>
2026-06-10 10:58:21 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Cegah context menu di seluruh halaman
+document.addEventListener('contextmenu', e => e.preventDefault())
+document.addEventListener('touchstart', e => {
+  if (e.touches.length > 1) e.preventDefault()
+}, { passive: false })
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
